40a947bb18b831f57825226ed2207e2b1cc01363
deisesilva <kinsk@msn.com>
2021-08-20 09:11:22 -0300

    inss value

--- src/module/calculations/interfaces/impl/EspCalculation.java
+++ src/module/calculations/interfaces/impl/EspCalculation.java
@@ -10,16 +10,32 @@ public class EspCalculation implements CalculationInterface {
 
     @Override
     public double inss(List<Integer> listOfValues) {
-        double salario = 0;
+        double salario;
+        double increValue;
+        double percent;
 
-        if(salario <= 1100){
+        double faixaA = 1100.01;
+        double faixaB = 2203.48;
+        double faixaC = 3305.22;
+        double faixaD = 6433.57;
+
+        double inss;
+
+
+
+
+        if(salario <= faixaA){
             //7,5%
 
-        }else if (salario > 1100.01 && salario<= 2203.48){
+        }else if (salario > faixaA && salario<= faixaB){
             //9%
-        }else if (salario > 2203.48 && salario<= 3305.22){
-            //12%
-        }else if (salario > 3305.22 && salario<= 6433.57){
+        }else if (salario > faixaB && salario<= faixaC){
+            percent = 12/100;
+            salario = 3000;
+            double increValue = (salario*percent);
+            inss = ((salario - faixaB).percent) + increValue;
+
+        }else if (salario > faixaC && salario<= faixaD ){
             //14%
         }else{
             System.out.print("This value doesn't work. It's so right");
