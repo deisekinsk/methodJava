06f2f9792158bec7821de48911f45550a36dea16
deisesilva <kinsk@msn.com>
2021-09-22 22:59:34 -0300

    Class super()

--- src/module/view/MenuView.java
+++ src/module/view/MenuView.java
@@ -3,34 +3,65 @@ package module.view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MenuView extends JFrame{
+public class MenuView extends JFrame implements ActionListener{
+
+    private FinancialView financialView;
+    private MathView mathView;
 
     //constructor
     public MenuView(){
+        super("Calculadora Matemática e Financeira.");
 
-        JFrame f= new JFrame("Calculadora Matemática e Financeira");
+        financialView = new FinancialView();
+        mathView = new MathView();
+
+
         JPanel panel=new JPanel();
-        panel.setBounds(100,250,200,200);
+        panel.setBounds(100,125,200,200);
 
         panel.setLayout(new FlowLayout());
         JLabel label = new JLabel("Escolha uma opção: ");
 
 
-        JButton b1=new JButton("Confirma");
-        b1.setBounds(100,100,80,30);
-        //b1.setBackground(Color.blue);
+        JButton b1=new JButton("Matemática");
+        b1.setBounds(0,0,80,30);
+
+        JButton b2=new JButton("Financeira");
+        b2.setBounds(0,0,80,30);
+
+        b1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mathView = new MathView();
+            }
+        });
+
+        b2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                financialView = new FinancialView();
+            }
+        });
 
         panel.add(label);
         panel.add(b1);
-        f.add(panel);
-        f.setSize(400,400);
-        f.setLayout(null);
+        panel.add(b2);
+        add(panel);
 
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setVisible(true);
+
+
+        setSize(400,400);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
 
     }
 
-
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+    }
 }
